8354333110f208a05d5dbc3c26edcfba7be5f20d
Devin Xie <devinxie418@gmail.com>
2026-04-10 22:08:08 -0400

    copy frontend code

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+// Screens
+import { LandingPage } from "./components/screens/LandingPage";
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import './App.css'
+
+function App() {
+  
+  return (
+    <>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={ <LandingPage/> } />
+        </Routes>
+      </BrowserRouter>
+    </>
+  )
+}
+
+export default App;
