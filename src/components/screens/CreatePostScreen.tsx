0d99a44a911d2c0ecd4af743b14776eca8be133e
Devin Xie <devinxie418@gmail.com>
2026-04-27 23:29:10 -0400

    add title input

--- src/components/screens/CreatePostScreen.tsx
+++ src/components/screens/CreatePostScreen.tsx
@@ -5,6 +5,8 @@ import { Editor } from '@tinymce/tinymce-react';
 // Components
 import { PageForbiddenScreen } from "./PageForbiddenScreen";
 import { useEffect, useState } from "react";
+import { Label } from "../ui/label";
+import { Input } from "../ui/input";
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
@@ -41,28 +43,39 @@ export function CreatePostScreen() {
     return (
         <>
         {apiKey && user && user.role === "AUTHOR" ? 
-        <div className="md:m-36 m-6">
-            <Editor
-                key={ darkMode ? "dark" : "light" }
-                apiKey={apiKey}
-                init={{
-                    menubar: false,
-                    statusbar: false,
-                    height: 600,
-                    plugins: [
-                        'advlist',
-                        'autolink',
-                        'lists',
-                        'link',
-                        'image',
-                        'table',
-                        'wordcount',
-                    ],
-                    toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline | align lineheight | checklist numlist bullist indent outdent | image',
-                    skin: darkMode ? 'oxide-dark' : 'oxide',
-                    content_css: darkMode ? 'tinymce-5-dark' : 'writer'
-                }}
-            />
+        <div className="md:mx-40 md:my-16 m-6 flex flex-col gap-4">
+            <div>
+                <Label className="text-xl">
+                    Title:
+                </Label>
+                <Input></Input>
+            </div>
+            <div>
+                <Label className="text-xl">
+                    Content:
+                </Label>
+                <Editor
+                    key={ darkMode ? "dark" : "light" }
+                    apiKey={apiKey}
+                    init={{
+                        menubar: false,
+                        statusbar: false,
+                        height: 600,
+                        plugins: [
+                            'advlist',
+                            'autolink',
+                            'lists',
+                            'link',
+                            'image',
+                            'table',
+                            'wordcount',
+                        ],
+                        toolbar: 'undo redo | blocks fontfamily fontsize | bold italic underline | align lineheight | checklist numlist bullist indent outdent | image',
+                        skin: darkMode ? 'oxide-dark' : 'oxide',
+                        content_css: darkMode ? 'tinymce-5-dark' : 'writer'
+                    }}
+                />
+            </div>
         </div>
         :
         <PageForbiddenScreen />
