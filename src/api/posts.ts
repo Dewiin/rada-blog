6041af052f8aa94f854e4259753e7123c4ef118b
Devin Xie <devinxie418@gmail.com>
2026-05-05 14:17:45 -0400

    feat: implement posts api layer

---
+++ src/api/posts.ts
@@ -0,0 +1,64 @@
+export async function fetchAllPosts(
+    setPosts: Function,
+    setError: Function,
+    setIsLoading: Function
+) {
+    setIsLoading(true);
+    try {
+        const VITE_API_URL = import.meta.env.VITE_API_URL;
+        const response = await fetch(`${VITE_API_URL}/api/posts`, {
+            method: "GET",
+            credentials: "include",
+        });
+        
+        if(!response.ok) {
+            setError({
+                title: "Error getting a response.",
+                description: "Please try again later."
+            });
+            return;
+        } 
+        const result = await response.json();
+        setPosts(result.posts);
+    } catch (err: any) {
+        setError({
+            title: "Server error",
+            description: "Please try again later."
+        });
+    } finally {
+        setIsLoading(false);
+    }
+}
+
+export async function fetchPost(
+    id: string,
+    setError: Function,
+    setPost: Function,
+    setIsLoading: Function
+) {
+    setIsLoading(true);
+    try {
+        const VITE_API_URL = import.meta.env.VITE_API_URL;
+        const response = await fetch(`${VITE_API_URL}/api/posts/${id}`, {
+            method: "GET",
+            credentials: "include",
+        });
+
+        if(!response.ok) {
+            setError({
+                title: "Error getting a response.",
+                description: "Please try again later."
+            });
+            return;
+        } 
+        const result = await response.json();
+        setPost(result.post);
+    } catch (err) {
+        setError({
+            title: "Server error",
+            description: "Please try again later."
+        });
+    } finally {
+        setIsLoading(false);
+    }
+}
