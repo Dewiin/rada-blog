c703d07efca4064b25d88e221378a5ea906a450c
Devin Xie <devinxie418@gmail.com>
2026-05-04 22:33:58 -0400

    add form error handling to createPostScreen

--- src/components/screens/CreatePostScreen.tsx
+++ src/components/screens/CreatePostScreen.tsx
@@ -1,60 +1,82 @@
 import { useAuth } from "@/contexts/AuthContext"
 import { useUI } from "@/contexts/UIContext";
-import { useState } from "react";
+import { Controller, useForm } from "react-hook-form";
+import { zodResolver } from "@hookform/resolvers/zod"
+import * as z from "zod"
 
 // Components
 import { PageForbiddenScreen } from "./PageForbiddenScreen";
 import { Label } from "@/components/ui/label";
 import { Input } from "@/components/ui/input";
-import type { Content } from "@tiptap/react"
 import { MinimalTiptapEditor } from "@/components/ui/minimal-tiptap"
 import { Button } from "../ui/button";
 import { 
     Field,
+    FieldGroup,
+    FieldError,
 } from "../ui/field";
 import { toast } from "sonner";
 
 const VITE_API_URL = import.meta.env.VITE_API_URL;
 
+const formSchema = z.object({
+    title: z
+        .string()
+        .min(10, "Title must be at least 10 characters.")
+        .max(100, "Title must be at most 100 characters."),
+    subtitle: z
+        .string()
+        .min(10, "Subtitle must be at least 10 characters.")
+        .max(100, "Description must be at most 100 characters."),
+    content: z
+        .string()
+        .min(100, "Content is too short!")
+});
+
 export function CreatePostScreen() {
-    const [title, setTitle] = useState<string>("");
-    const [subtitle, setSubtitle] = useState<string>("");
-    const [value, setValue] = useState<Content>("");
     const { user, refreshToken } = useAuth();
     const { isLoading, setIsLoading } = useUI();
 
-    async function onSubmit() {
-        setIsLoading(true);
-        try {
-            await toast.promise(
-                async () => {
-                    const data = {
-                        title,
-                        subtitle,
-                        content: value,
+    const form = useForm<z.infer<typeof formSchema>>({
+        resolver: zodResolver(formSchema),
+        defaultValues: {
+            title: "",
+            subtitle: "",
+            content: ""
+        },
+        mode: "onChange",
+    })
+
+    async function onSubmit(data: z.infer<typeof formSchema>) {
+        await toast.promise(
+            async () => {
+                try {
+                    setIsLoading(true);
+                    const body = {
+                        ...data,
                         published: true,
                         authorId: user?.id
                     };
-    
+                    
                     let response = await fetch(`${VITE_API_URL}/api/posts`, {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(data),
+                        body: JSON.stringify(body),
                         credentials: "include",
                     });
-
+                    
                     if(response.status === 401) {
                         console.log("refreshing");
                         await refreshToken();
                         response = await fetch(`${VITE_API_URL}/api/posts`, {
                             method: "POST",
                             headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify(data),
+                            body: JSON.stringify(body),
                             credentials: "include",
                         });
                     }
                     const result = await response.json();
-    
+                    
                     if(!response.ok) {
                         toast.warning(result.error, {
                             position: "top-center",
@@ -66,33 +88,31 @@ export function CreatePostScreen() {
                             description: "View the new post on the home page."
                         })
                     }
-                }, {
-                    position: "top-center",
-                    loading: "Submitting post...",
+                } finally {
+                    setIsLoading(false);
                 }
-            );
-        } finally {
-            setIsLoading(false);
-        }
+            }, {
+                position: "top-center",
+                loading: "Submitting post...",
+            }
+        );
     }
 
-    async function onSave() {
-        setIsLoading(true);
-        try {
-            await toast.promise(
-                async () => {
-                    const data = {
-                        title,
-                        subtitle,
-                        content: value,
+    async function onSave(data: z.infer<typeof formSchema>) {
+        await toast.promise(
+            async () => {
+                try {
+                    setIsLoading(true);
+                    const body = {
+                        ...data,
                         published: false,
                         authorId: user?.id
                     };
-    
+                    
                     let response = await fetch(`${VITE_API_URL}/api/posts`, {
                         method: "POST",
                         headers: { "Content-Type": "application/json" },
-                        body: JSON.stringify(data),
+                        body: JSON.stringify(body),
                         credentials: "include",
                     });
                     if(response.status === 401) {
@@ -101,12 +121,12 @@ export function CreatePostScreen() {
                         response = await fetch(`${VITE_API_URL}/api/posts`, {
                             method: "POST",
                             headers: { "Content-Type": "application/json" },
-                            body: JSON.stringify(data),
+                            body: JSON.stringify(body),
                             credentials: "include",
                         });
                     }
                     const result = await response.json();
-    
+                    
                     if(!response.ok) {
                         toast.warning(result.error, {
                             position: "top-center",
@@ -118,14 +138,14 @@ export function CreatePostScreen() {
                             description: "View saved posts in your profile."
                         });
                     }
-                }, {
-                    position: "top-center",
-                    loading: "Saving post...",
+                } finally {
+                    setIsLoading(false);
                 }
-            );
-        } finally {
-            setIsLoading(false);
-        }
+            }, {
+                position: "top-center",
+                loading: "Saving post...",
+            }
+        );
     }
 
     return (
@@ -135,42 +155,74 @@ export function CreatePostScreen() {
             <Label className="text-5xl font-extrabold">
                 Create a Post
             </Label>
-            <div
-                className="flex flex-col gap-6"
+            <form 
+                className="flex flex-col gap-8"
+                onSubmit={form.handleSubmit(onSubmit)}
             >
-                <Field 
-                    className="flex flex-col gap-1"
-                >
-                    <Input 
-                        onChange={(e) => setTitle(e.target.value)}
-                        className="bg-input/30 font-extrabold"
-                        placeholder="My Title"
-                        aria-invalid={title.length === 0}
+                <FieldGroup className="gap-8">
+                    <div className="flex flex-col gap-2">
+                        <Controller
+                            name="title"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid} className="gap-1">
+                                    <Input
+                                        {...field}
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="My Title"
+                                        autoComplete="off"
+                                        className="font-extrabold"
+                                        />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                            />
+                        <Controller
+                            name="subtitle"
+                            control={form.control}
+                            render={({ field, fieldState }) => (
+                                <Field data-invalid={fieldState.invalid} className="gap-1">
+                                    <Input
+                                        {...field}
+                                        aria-invalid={fieldState.invalid}
+                                        placeholder="Write a preview subtitle..."
+                                        autoComplete="off"
+                                        className="font-thin"
+                                        />
+                                    {fieldState.invalid && (
+                                        <FieldError errors={[fieldState.error]} />
+                                    )}
+                                </Field>
+                            )}
+                        />
+                    </div>
+                    <Controller
+                        name="content"
+                        control={form.control}
+                        render={({ field, fieldState }) => (
+                            <Field data-invalid={fieldState.invalid}>
+                                <MinimalTiptapEditor
+                                    {...field}
+                                    className="w-full"
+                                    editorContentClassName="p-5 bg-input/30"
+                                    output="html"
+                                    placeholder="Enter your description..."
+                                    autofocus={true}
+                                    editable={true}
+                                    editorClassName="focus:outline-hidden"
+                                />
+                                {fieldState.invalid && (
+                                    <FieldError errors={[fieldState.error]} />
+                                )}
+                            </Field>
+                        )}
                     />
-                    <Input 
-                        onChange={(e) => setSubtitle(e.target.value)}
-                        className="bg-input/30"
-                        placeholder="Write a preview subtitle..."
-                        aria-invalid={subtitle.length === 0}
-                    />
-                </Field>
-                <div>
-                    <MinimalTiptapEditor
-                        value={value}
-                        onChange={setValue}
-                        className="w-full"
-                        editorContentClassName="p-5 bg-input/30"
-                        output="html"
-                        placeholder="Enter your description..."
-                        autofocus={true}
-                        editable={true}
-                        editorClassName="focus:outline-hidden"
-                    />
-                </div>
+                </FieldGroup>
                 <div className="flex gap-2">
                     <Button 
                         className="w-fit cursor-pointer"
-                        onClick={() => onSubmit()}
                         disabled={isLoading}
                         type="submit"
                     >
@@ -179,14 +231,14 @@ export function CreatePostScreen() {
                     <Button 
                         className="w-fit cursor-pointer"
                         variant={"secondary"}
-                        onClick={() => onSave()}
+                        onClick={form.handleSubmit(onSave)}
                         disabled={isLoading}
-                        type="submit"
+                        type="button"
                     >
                         Save
                     </Button>
                 </div>
-            </div>
+            </form>
         </div>
         :
         <PageForbiddenScreen />
