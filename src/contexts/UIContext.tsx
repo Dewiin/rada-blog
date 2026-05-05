abaf7b9d73ae500fefd10bb601c97a436e4ed14e
Devin Xie <devinxie418@gmail.com>
2026-05-05 14:17:13 -0400

    feat: add isSuccess context

--- src/contexts/UIContext.tsx
+++ src/contexts/UIContext.tsx
@@ -1,7 +1,7 @@
 import React, { createContext, useState, useContext, useEffect } from 'react';
 
 // types
-import type { IError } from '@/components/types/Error';
+import type { IError, ISuccess } from '@/components/types/Toast';
 import { toast } from 'sonner';
 
 type UIContextProps = {
@@ -9,18 +9,23 @@ type UIContextProps = {
     setIsLoading: (isLoading: boolean) => void,
     error: IError | undefined,
     setError: (error: IError) => void,
+    success: ISuccess | undefined,
+    setSuccess: (success: ISuccess) => void,
 }
 
 const UIContext = createContext<UIContextProps>({
     isLoading: false,
     setIsLoading: () => {},
     error: undefined,
-    setError: () => {}
+    setError: () => {},
+    success: undefined,
+    setSuccess: () => {},
 });
 
 export default function UIProvider({ children }: { children: React.ReactNode }) {
     const [isLoading, setIsLoading] = useState<boolean>(false);
     const [error, setError] = useState<IError | undefined>();
+    const [success, setSuccess] = useState<ISuccess | undefined>();
 
     useEffect(() => {
         if(error) {
@@ -29,12 +34,22 @@ export default function UIProvider({ children }: { children: React.ReactNode }) 
             });
         }
     }, [error]);
+    
+    useEffect(() => {
+        if(success) {
+            toast.success(success?.title, {
+                description: success?.description
+            });
+        }
+    }, [success]);
 
     const values = {
         isLoading,
         setIsLoading,
         error,
-        setError
+        setError,
+        success,
+        setSuccess
     }
 
     return (
