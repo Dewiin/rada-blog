34252a1d30aac694296273a96456df88747de0ea
Devin Xie <devinxie418@gmail.com>
2026-05-05 20:10:46 -0400

    feat: 401 page unauthorized

---
+++ src/components/screens/PageUnauthorizedScreen.tsx
@@ -0,0 +1,7 @@
+export function PageNotFoundScreen() {
+    return (
+        <h1 className="grid h-8/10 text-md place-content-center">
+            401 Unauthorized Request.
+        </h1>
+    )
+}
