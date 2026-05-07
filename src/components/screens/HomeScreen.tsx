7cffc398e6afe43bdedafcb3eb3aedd21d67c53f
Devin Xie <devinxie418@gmail.com>
2026-05-06 22:56:21 -0400

    chore: add div keys and use skeleton template

--- src/components/screens/HomeScreen.tsx
+++ src/components/screens/HomeScreen.tsx
@@ -3,16 +3,16 @@ import { useState, useEffect } from "react";
 // api
 import { fetchAllPosts } from "@/api/posts";
 
-// types
-import type { IPost } from "../types/Post";
-
 // components
-import { PostPreview } from "../postPreview/PostPreview";
-import { Separator } from "../ui/separator";
+import { Separator } from "@/components/ui/separator";
+import { PostPreview } from "@/components/postPreview/PostPreview";
+import { SkeletonPostPreview } from "@/components/skeleton/SkeletonPostPreview";
 
 // contexts
 import { useUI } from "@/contexts/UIContext";
-import { Skeleton } from "../ui/skeleton";
+
+// types
+import type { IPost } from "@/components/types/Post";
 
 export function HomeScreen() {
     const [ posts, setPosts ] = useState<IPost[]>([]);
@@ -30,41 +30,17 @@ export function HomeScreen() {
         >
             { isLoading && 
                 // skeleton preview
-                <div 
-                    className="flex flex-col gap-4 m-4 h-fit"
-                >   
-                    {/* author */}
-                    <div className="flex gap-2 items-center">
-                        <Skeleton className="h-6 w-6 rounded-full" />
-                        <Skeleton className="h-4 w-20 rounded-full" />
-                    </div>
-
-                    {/* title and subtitle */}
-                    <div className="flex flex-col gap-2">
-                        {/* title */}
-                        <div className="flex flex-col gap-1">
-                            <Skeleton className="h-6 md:w-[85%] rounded-full" />
-                            <Skeleton className="h-6 md:w-[70%] w-[85%] rounded-full" />
-                        </div>
-
-                        {/* subtitle */}
-                        <div className="flex flex-col gap-1">
-                            <Skeleton className="h-3 w-[90%] rounded-full" />
-                            <Skeleton className="h-3 w-[60%] rounded-full" />
-                        </div>
-                    </div>
-
-                    <Skeleton className="h-4 md:w-60 w-30 rounded-full" />
-                </div>
+                <SkeletonPostPreview />
             }
             { !isLoading && posts.length > 0 && posts.map((post, index) => (
-                <>
+                <div
+                    key={post.id}
+                >
                     <PostPreview
-                        key={index}
                         post={post} 
                     />
                     {index < posts.length-1 && <Separator />}
-                </>
+                </div>
             ))}
         </div>
     )
