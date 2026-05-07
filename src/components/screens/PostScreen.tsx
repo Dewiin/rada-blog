dbadc1632464568d1daa247b841f392ab15fb4ff
Devin Xie <devinxie418@gmail.com>
2026-05-06 22:41:01 -0400

    use skeleton template, and reroute to 403 page if user is not author of an unpublished post

--- src/components/screens/PostScreen.tsx
+++ src/components/screens/PostScreen.tsx
@@ -7,118 +7,104 @@ import { fetchPost } from "@/api/posts";
 import { formatDate } from "@/helpers/formatDate";
 
 // types
-import type { IPost } from "../types/Post";
+import type { IPost } from "@/components/types/Post";
+
+// components
+import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Separator } from "../ui/separator";
+import { SkeletonPostScreen } from "@/components/skeleton/SkeletonPostScreen";
+import { FaArrowLeft, FaRegComment } from "react-icons/fa6";
+import { PiHandsClappingLight } from "react-icons/pi";
+import { PageForbiddenScreen } from "./PageForbiddenScreen";
 
 // contexts
 import { useUI } from "@/contexts/UIContext";
-
-// components
-import { Avatar, AvatarFallback, AvatarImage } from "../ui/avatar";
-import { Skeleton } from "../ui/skeleton";
-import { Separator } from "../ui/separator";
-import { FaArrowLeft, FaRegComment } from "react-icons/fa6";
-import { PiHandsClappingLight } from "react-icons/pi";
+import { useAuth } from "@/contexts/AuthContext";
 
 export function PostScreen() {
     const { id } = useParams();
     const [ post, setPost ] = useState<IPost | null>(null);
     const { setError, isLoading, setIsLoading } = useUI();
+    const { user } = useAuth();
     const navigate = useNavigate();
 
     useEffect(() => {
         if(id) fetchPost(id, setError, setPost, setIsLoading);
     }, []);
 
+    if( !post?.published && (post?.author.id !== user?.id)) {
+        return <PageForbiddenScreen />
+    }
+
     return (
-        <div className="flex md:flex-row flex-col md:gap-20 gap-6 md:mx-64 md:my-24 m-12">
-            <div>
-                <FaArrowLeft 
-                    onClick={() => navigate(-1)}
-                    size={24}  
-                    className="cursor-pointer md:mt-2"
-                />
-            </div>
-
-            {/* content */}
-            <div className="w-full flex flex-col gap-12">
-                {/* header */}
-                <div className="flex flex-col gap-4">
-
-                    { isLoading && 
-                    <>
-                        {/* title */}
-                        <div className="flex flex-col gap-2">
-                            <Skeleton className="h-8 md:w-[85%] rounded-full" /> 
-                            <Skeleton className="h-8 md:w-[70%] w-[85%] rounded-full" /> 
-                        </div>
-
-                        <div className="flex flex-col gap-2">
-                            <Skeleton className="h-4 w-[80%] rounded-full" />
-                            <Skeleton className="h-4 w-[70%] rounded-full" />
-                        </div>
-
-                        <div className="flex gap-2 items-center">
-                            <Skeleton className="h-8 w-8 rounded-full" />
-                            <Skeleton className="h-4 w-20 rounded-full" />
-                            <Skeleton className="h-4 w-20 rounded-full" />
-                        </div>
-
-                        <Separator />
-                                <Skeleton className="h-4 w-30 rounded-full" />
-                        <Separator />
-                    </>
-                    }
-
-                    { !isLoading && 
-                    <>
-                        {/* title */}
-                        <p className="text-3xl font-bold">
-                            {post?.title}
-                        </p>
-
-                        {/* subtitle */}
-                        <p className="dark:text-stone-500 text-stone-600">
-                            {post?.subtitle}
-                        </p>
-
-                        {/* author */}
-                        <div className="flex gap-2 items-center text-sm">
-                            <Avatar size="default">
-                                <AvatarImage alt={`@${post?.author.username}`} />
-                                <AvatarFallback>{post?.author.username.substring(0,2)}</AvatarFallback>
-                            </Avatar>
-                            <p>
-                                {post?.author.username} · <span className="dark:text-stone-500 text-stone-600">{post && formatDate(post.createdAt)}</span>
-                            </p>
-                        </div>
-
-                        <Separator />
-                            {/* claps and comments */}
-                            <div className="flex gap-8 mx-2">
-                                <div className="flex items-center gap-2">
-                                    <PiHandsClappingLight />
-                                    {post?.claps}
-                                </div>
-                                <div className="flex items-center gap-2">
-                                    <FaRegComment />
-                                    {post?.comments.length}
-                                </div>
-                            </div>
-                        <Separator />
-                    </>
-                    }
+        <>
+            <div className="flex md:flex-row flex-col md:gap-20 gap-6 md:mx-64 md:my-24 m-12">
+                <div>
+                    <FaArrowLeft 
+                        onClick={() => navigate(-1)}
+                        size={24}  
+                        className="cursor-pointer md:mt-2"
+                    />
                 </div>
 
-                {/* body */}
-                {post && 
-                <div
-                    dangerouslySetInnerHTML={{
-                        __html: DOMPurify.sanitize(post.content)
-                    }}
-                    className="text-lg/8"
-                />
-                }
+                {/* content */}
+                <div className="w-full flex flex-col gap-12">
+                    {/* header */}
+                    <div className="flex flex-col gap-4">
+
+                        { isLoading && <SkeletonPostScreen /> }
+
+                        { !isLoading && 
+                        <>
+                            {/* title */}
+                            <p className="text-3xl font-bold">
+                                {post?.title}
+                            </p>
+
+                            {/* subtitle */}
+                            <p className="dark:text-stone-500 text-stone-600">
+                                {post?.subtitle}
+                            </p>
+
+                            {/* author */}
+                            <div className="flex gap-2 items-center text-sm">
+                                <Avatar size="default">
+                                    <AvatarImage alt={`@${post?.author.username}`} />
+                                    <AvatarFallback>{post?.author.username.substring(0,2)}</AvatarFallback>
+                                </Avatar>
+                                <p>
+                                    {post?.author.username} · <span className="dark:text-stone-500 text-stone-600">{post && formatDate(post.createdAt)}</span>
+                                </p>
+                            </div>
+
+                            <Separator />
+                                {/* claps and comments */}
+                                <div className="flex gap-8 mx-2">
+                                    <div className="flex items-center gap-2">
+                                        <PiHandsClappingLight />
+                                        {post?.claps}
+                                    </div>
+                                    <div className="flex items-center gap-2">
+                                        <FaRegComment />
+                                        {post?.comments.length}
+                                    </div>
+                                </div>
+                            <Separator />
+                        </>
+                        }
+                    </div>
+
+                    {/* body */}
+                    {post && 
+                    <div
+                        dangerouslySetInnerHTML={{
+                            __html: DOMPurify.sanitize(post.content)
+                        }}
+                        className="text-lg/8"
+                    />
+                    }
+                </div>
             </div>
-        </div>
+        </>
     )
 }
