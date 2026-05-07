0baee2964133713b81f32e01dc65d5c0fa021aa2
Devin Xie <devinxie418@gmail.com>
2026-05-06 22:38:28 -0400

    implement client api layer

---
+++ src/api/client.ts
@@ -0,0 +1,31 @@
+const VITE_API_URL = import.meta.env.VITE_API_URL;
+
+export async function api(
+    path: string,
+    options: RequestInit = {},
+    refreshToken: Function,
+    setError: Function,
+) {
+    let response = await fetch(`${VITE_API_URL}${path}`, {
+        ...options,
+        credentials: "include"
+    });
+
+    if(response.status === 401) {
+        await refreshToken();
+        response = await fetch(`${VITE_API_URL}${path}`, {
+            ...options,
+            credentials: "include"
+        });
+    }
+
+    let result = await response.json();
+    if (!response.ok) {
+        setError({
+            title: result?.error,
+        });
+        result = null;
+    }
+
+    return result;
+}
